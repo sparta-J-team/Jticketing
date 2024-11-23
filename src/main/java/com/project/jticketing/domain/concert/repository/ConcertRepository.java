80e46b6642a2bbfa6a269469b6d4a7f0103a8125
soung90 <sounglikane@gmail.com>
2024-11-23 18:09:44 +0900

    refactor(concert-repository): Query method 추가 #5

--- src/main/java/com/project/jticketing/domain/concert/repository/ConcertRepository.java
+++ src/main/java/com/project/jticketing/domain/concert/repository/ConcertRepository.java
@@ -2,6 +2,12 @@ package com.project.jticketing.domain.concert.repository;
 
 import com.project.jticketing.domain.concert.entity.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
+    @Query("SELECT c FROM Concert c WHERE c.place.id = :placeId AND c.startTime = :startTime")
+    Optional<Concert> findByPlaceAndStartTime(@Param("placeId") Long placeId, @Param("startTime") String startTime);
 }
