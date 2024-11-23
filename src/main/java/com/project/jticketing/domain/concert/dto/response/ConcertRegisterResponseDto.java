6e077b6c92c6eb18dea7cd26984f9e0544c37657
soung90 <sounglikane@gmail.com>
2024-11-23 17:43:16 +0900

    feat(concert-dto): response dto 구현 #5

---
+++ src/main/java/com/project/jticketing/domain/concert/dto/response/ConcertRegisterResponseDto.java
@@ -0,0 +1,12 @@
+package com.project.jticketing.domain.concert.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConcertRegisterResponseDto {
+    private String message;
+}
