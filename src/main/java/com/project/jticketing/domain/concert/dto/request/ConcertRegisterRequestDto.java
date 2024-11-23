d20cab070b5405438be37a33820be0acae6ad5a1
soung90 <sounglikane@gmail.com>
2024-11-23 17:43:03 +0900

    feat(concert-dto): request dto 구현  #5

---
+++ src/main/java/com/project/jticketing/domain/concert/dto/request/ConcertRegisterRequestDto.java
@@ -0,0 +1,35 @@
+package com.project.jticketing.domain.concert.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConcertRegisterRequestDto {
+
+    @NotBlank(message = "콘서트 제목을 입력하세요")
+    private String title;
+
+    @NotNull(message = "날짜를 입력하세요")
+    @Size(min = 1, message = "최소한 하나의 콘서트 날짜를 입력하세요")
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜는 yyyy-MM-dd 형식이어야 합니다.")
+    private List<String> eventsDate;
+
+    @NotBlank(message = "시작 시간을 입력하세요")
+    private String startTime;
+
+    @NotBlank(message = "끝 시간을 입력하세요")
+    private String endTime;
+
+    @NotNull(message = "장소 id를 입력하세요")
+    private Long PlaceId;
+
+    @NotNull(message = "가격을 입력하세요")
+    @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
+    private Long Price;
+}
