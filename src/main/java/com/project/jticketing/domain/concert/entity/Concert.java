9f372e28f346737849753e981e53ec37fe801601
soung90 <sounglikane@gmail.com>
2024-11-24 22:09:34 +0900

    feat(concert-entity): Concert 수정 method 구현 #5

--- src/main/java/com/project/jticketing/domain/concert/entity/Concert.java
+++ src/main/java/com/project/jticketing/domain/concert/entity/Concert.java
@@ -6,7 +6,11 @@ import com.project.jticketing.domain.place.entity.Place;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -36,11 +40,23 @@ public class Concert extends Timestamped {
     @OneToMany(mappedBy = "concert", fetch = FetchType.LAZY)
     private List<Event> events;
 
-    public Concert (String title, String startTime, String endTime, Long price, Place place) {
+    public void update(String title, String startTime, String endTime, Long price, Place place, List<String> eventsDate) {
         this.title = title;
         this.startTime = startTime;
         this.endTime = endTime;
         this.price = price;
         this.place = place;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        List<Event> updatedEvents = eventsDate.stream()
+                .map(eventDate -> Event.builder()
+                        .concertDate(LocalDateTime.from(LocalDate.parse(eventDate, formatter)))
+                        .concert(this)
+                        .build())
+                .toList();
+
+        this.events.clear();
+        this.events.addAll(updatedEvents);
     }
+
 }
