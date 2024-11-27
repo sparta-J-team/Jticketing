8fa8103b4a28f00bf43744b6bc7fc9e6e8c38635
caose <rlagkrwls258@gmail.com>
2024-11-27 13:46:56 +0900

    feat : 예매 컨트롤러 구현

--- src/main/java/com/project/jticketing/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/project/jticketing/domain/reservation/controller/ReservationController.java
@@ -1,4 +1,31 @@
 package com.project.jticketing.domain.reservation.controller;
 
+import com.project.jticketing.config.security.UserDetailsImpl;
+import com.project.jticketing.domain.reservation.dto.request.ReservationRequestDTO;
+import com.project.jticketing.domain.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping("/event/{eventId}")
+    public ResponseEntity<Boolean> reservationSeat(
+            @AuthenticationPrincipal UserDetailsImpl authUser,
+            @PathVariable Long eventId,
+            @RequestBody ReservationRequestDTO reservationRequestDTO ) {
+
+        return new ResponseEntity<Boolean>(
+                reservationService.reserveSeatWithRedis(authUser,eventId,reservationRequestDTO.getSeatNum()),
+                HttpStatus.OK);
+    }
+
 }
